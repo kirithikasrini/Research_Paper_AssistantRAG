b47395bc99bcc864f941403b208afa1c850c79c1
kirithikasrini <kirithikasrini04@gmail.com>
2026-02-15 20:59:24 +0530

    feat: Integrate HuggingFace Flan-T5 into the RAG pipeline.

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -1,19 +1,24 @@
+# rag_pipeline.py
+
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatOpenAI
+from langchain.llms import HuggingFacePipeline
+
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+
 
 def create_vectorstore(pdf_path):
     # Load PDF
     loader = PyPDFLoader(pdf_path)
     documents = loader.load()
 
-    # Split into chunks
+    # Split text
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=200
+        chunk_size=500,
+        chunk_overlap=100
     )
     chunks = text_splitter.split_documents(documents)
 
@@ -26,11 +31,23 @@ def create_vectorstore(pdf_path):
     return vectorstore
 
 
-def create_qa_chain(vectorstore, openai_api_key):
-    llm = ChatOpenAI(
-        openai_api_key=openai_api_key,
-        model="gpt-3.5-turbo"
+def create_qa_chain(vectorstore):
+
+    # Load free LLM
+    model_name = "google/flan-t5-small"
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+    pipe = pipeline(
+        "text2text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        max_length=512,
+        temperature=0
     )
+
+    llm = HuggingFacePipeline(pipeline=pipe)
 
     qa_chain = RetrievalQA.from_chain_type(
         llm=llm,
